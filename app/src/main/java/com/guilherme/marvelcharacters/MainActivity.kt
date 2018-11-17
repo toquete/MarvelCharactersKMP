bbc76e2039ba23e1cd25f0ed077e1af8be98e594
Guilherme Toquete <guilherme.toquete@gmail.com>
2018-11-17 17:09:30 -0200

    Enabling the search widget

--- app/src/main/java/com/guilherme/marvelcharacters/MainActivity.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.guilherme.marvelcharacters
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.SearchView
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu?.findItem(R.id.search_bar)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
     }
 }
