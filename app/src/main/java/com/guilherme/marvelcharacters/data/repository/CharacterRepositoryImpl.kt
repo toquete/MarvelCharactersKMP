1247a9bf737ba40c53ff28f419a3720ff5f931d1
Guilherme Rogério Toquete <gtoquete@ciandt.com>
2019-05-21 13:32:26 -0300

    Adicionando contexto de IO para chamada de API

--- app/src/main/java/com/guilherme/marvelcharacters/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/data/repository/CharacterRepositoryImpl.kt
@@ -1,18 +1,21 @@
 package com.guilherme.marvelcharacters.data.repository
 
 import com.guilherme.marvelcharacters.BuildConfig
-import com.guilherme.marvelcharacters.data.model.Character
 import com.guilherme.marvelcharacters.data.source.remote.Api
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils
 
 class CharacterRepositoryImpl(private val api: Api) : CharacterRepository {
 
-    override suspend fun getCharacters(name: String): List<Character> {
+    override suspend fun getCharacters(name: String) = withContext(Dispatchers.IO) {
         val ts = System.currentTimeMillis().toString()
         val hash = String(Hex.encodeHex(DigestUtils.md5(ts + BuildConfig.MARVEL_PRIVATE_KEY + BuildConfig.MARVEL_KEY)))
 
-        val result = api.getCharacters(ts, hash, BuildConfig.MARVEL_KEY, name).await()
-        return result.container.characters
+        api.getCharacters(ts, hash, BuildConfig.MARVEL_KEY, name)
+            .await()
+            .container
+            .characters
     }
 }
