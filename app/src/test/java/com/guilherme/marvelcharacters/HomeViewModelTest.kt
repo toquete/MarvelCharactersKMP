1cfd8eed3acc354bf90fe0e1d1f10c2cc861f476
Guilherme Toquete <guilherme.toquete@gmail.com>
2020-04-26 19:34:38 -0300

    Correcao testes unitarios

--- app/src/test/java/com/guilherme/marvelcharacters/HomeViewModelTest.kt
+++ app/src/test/java/com/guilherme/marvelcharacters/HomeViewModelTest.kt
@@ -2,16 +2,17 @@ package com.guilherme.marvelcharacters
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateHandle
+import com.google.common.truth.Truth.assertThat
 import com.guilherme.marvelcharacters.data.model.Character
 import com.guilherme.marvelcharacters.data.model.Image
 import com.guilherme.marvelcharacters.data.repository.CharacterRepository
 import com.guilherme.marvelcharacters.infrastructure.TestCoroutineRule
 import com.guilherme.marvelcharacters.ui.home.HomeViewModel
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
+import com.guilherme.marvelcharacters.util.getOrAwaitValue
+import com.guilherme.marvelcharacters.util.observeForTesting
+import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.mockk
-import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
@@ -23,10 +24,10 @@ class HomeViewModelTest {
     lateinit var viewModel: HomeViewModel
 
     @RelaxedMockK
-    lateinit var statesObserver: Observer<HomeViewModel.CharacterListState>
+    lateinit var characterRepository: CharacterRepository
 
     @RelaxedMockK
-    lateinit var characterRepository: CharacterRepository
+    lateinit var savedStateHandle: SavedStateHandle
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
@@ -37,13 +38,11 @@ class HomeViewModelTest {
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        viewModel = HomeViewModel(characterRepository, testCoroutineRule.testCoroutineDispatcher, mockk(relaxed = true)).apply {
-            states.observeForever(statesObserver)
-        }
+        viewModel = HomeViewModel(characterRepository, testCoroutineRule.testCoroutineDispatcher, savedStateHandle)
     }
 
     @Test
-    fun onSearchCharacter_getCharacterList_success() = testCoroutineRule.runBlockingTest {
+    fun `onSearchCharacter - envia estado de sucesso ao carregar a lista`() = testCoroutineRule.runBlockingTest {
         val character = Character(0, "Spider-Man", "The Amazing Spider-Man", Image("", ""))
         val characterList = listOf(character)
 
@@ -51,27 +50,45 @@ class HomeViewModelTest {
 
         viewModel.onSearchCharacter("spider")
 
-        verify { statesObserver.onChanged(HomeViewModel.CharacterListState.Characters(characterList)) }
+        assertThat(viewModel.states.getOrAwaitValue()).isEqualTo(HomeViewModel.CharacterListState.Characters(characterList))
     }
 
     @Test
-    fun onSearchCharacter_getCharacterList_emptyList() = testCoroutineRule.runBlockingTest {
+    fun `onSearchCharacter - envia estado vazio ao carregar uma lista vazia`() = testCoroutineRule.runBlockingTest {
         val characterList = emptyList<Character>()
 
         coEvery { characterRepository.getCharacters(any()) } returns characterList
 
         viewModel.onSearchCharacter("spider")
 
-        verify { statesObserver.onChanged(HomeViewModel.CharacterListState.EmptyState) }
+        assertThat(viewModel.states.getOrAwaitValue()).isEqualTo(HomeViewModel.CharacterListState.EmptyState)
     }
 
     @Test
-    fun onSearchCharacter_getCharacterList_error() = testCoroutineRule.runBlockingTest {
+    fun `onSearchCharacter - envia estado de erro`() = testCoroutineRule.runBlockingTest {
         val exception = Exception("This is an error")
         coEvery { characterRepository.getCharacters(any()) } throws exception
 
         viewModel.onSearchCharacter("spider")
 
-        verify { statesObserver.onChanged(HomeViewModel.CharacterListState.ErrorState(exception)) }
+        assertThat(viewModel.states.getOrAwaitValue()).isEqualTo(HomeViewModel.CharacterListState.ErrorState(exception))
+    }
+
+    @Test
+    fun `onSearchCharacter - envia estado de carregamento da tela`() = testCoroutineRule.runBlockingTest {
+        val character = Character(0, "Spider-Man", "The Amazing Spider-Man", Image("", ""))
+        val characterList = listOf(character)
+        val observer = mockk<Observer<Boolean>>(relaxed = true)
+
+        coEvery { characterRepository.getCharacters(any()) } returns characterList
+
+        viewModel.showLoading.observeForTesting(observer) {
+            viewModel.onSearchCharacter("spider")
+
+            verifySequence {
+                observer.onChanged(true)
+                observer.onChanged(false)
+            }
+        }
     }
 }
