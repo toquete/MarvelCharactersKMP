946c12a98e23cc1e2df4b4ac07117e5fb209a4aa
Guilherme Toquete <guilherme.toquete@gmail.com>
2021-11-04 18:30:46 -0300

    Removendo import não utilizado

--- app/src/androidTest/java/com/guilherme/marvelcharacters/infrastructure/TestModules.kt
+++ app/src/androidTest/java/com/guilherme/marvelcharacters/infrastructure/TestModules.kt
@@ -11,7 +11,6 @@ import com.guilherme.marvelcharacters.ui.detail.DetailViewModel
 import com.guilherme.marvelcharacters.ui.favorites.FavoritesViewModel
 import com.guilherme.marvelcharacters.ui.home.HomeViewModel
 import io.mockk.mockk
-import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
