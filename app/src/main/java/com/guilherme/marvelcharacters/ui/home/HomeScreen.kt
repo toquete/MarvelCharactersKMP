0531034484b820f9f76803cc2e7c7b9154ae4fc0
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-05-13 15:12:44 -0300

    Removendo botão Search

--- app/src/main/java/com/guilherme/marvelcharacters/ui/home/HomeScreen.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/ui/home/HomeScreen.kt
@@ -1,19 +1,19 @@
 package com.guilherme.marvelcharacters.ui.home
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
@@ -29,7 +29,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
@@ -40,6 +42,7 @@ import com.guilherme.marvelcharacters.R
 import com.guilherme.marvelcharacters.model.CharacterVO
 import com.guilherme.marvelcharacters.model.ImageVO
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun HomeScreen(
     state: HomeState,
@@ -47,44 +50,35 @@ fun HomeScreen(
     onItemClick: (CharacterVO) -> Unit
 ) {
     var query by rememberSaveable { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(modifier = Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier
-                .padding(all = 8.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            OutlinedTextField(
-                modifier = Modifier.weight(0.7f),
-                label = { Text(text = stringResource(id = R.string.character)) },
-                value = query,
-                onValueChange = { query = it },
-                trailingIcon = {
-                    if (query.isNotEmpty()) {
-                        IconButton(onClick = { query = "" }) {
-                            Icon(imageVector = Icons.Default.Cancel, contentDescription = null)
-                        }
+        OutlinedTextField(
+            modifier = Modifier.wrapContentHeight()
+                .fillMaxWidth()
+                .padding(8.dp),
+            label = { Text(text = stringResource(id = R.string.character)) },
+            value = query,
+            onValueChange = { query = it },
+            trailingIcon = {
+                if (query.isNotEmpty()) {
+                    IconButton(onClick = { query = "" }) {
+                        Icon(imageVector = Icons.Default.Cancel, contentDescription = null)
                     }
-                },
-                singleLine = true,
-                keyboardActions = KeyboardActions(
-                    onSearch = { onSearchButtonClick(query) }
-                ),
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
-            )
-            Button(
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .weight(0.3f),
-                onClick = { onSearchButtonClick(query) }
-            ) {
-                Text(text = stringResource(R.string.search).uppercase())
-            }
-        }
-        Column(
+                }
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchButtonClick(query)
+                    keyboardController?.hide()
+                }
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
+        )
+        Box(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            contentAlignment = Alignment.Center
         ) {
             when {
                 state.isLoading -> CircularProgressIndicator()
@@ -117,12 +111,14 @@ fun CharacterItem(
     onItemClick: (CharacterVO) -> Unit
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .height(48.dp)
             .clickable { onItemClick(character) }
     ) {
         Text(
-            modifier = Modifier.wrapContentSize()
+            modifier = Modifier
+                .wrapContentSize()
                 .padding(horizontal = 16.dp, vertical = 12.dp),
             text = character.name
         )
