90c00fe36f720cc48d068bbc5934d30caa0457df
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-05-19 16:51:57 -0300

    Implementando use case de busca por character por id

--- domain/src/main/java/com/guilherme/marvelcharacters/domain/usecase/GetFavoriteCharacterById.kt
+++ domain/src/main/java/com/guilherme/marvelcharacters/domain/usecase/GetFavoriteCharacterById.kt
@@ -3,8 +3,9 @@ package com.guilherme.marvelcharacters.domain.usecase
 import com.guilherme.marvelcharacters.domain.model.Character
 import com.guilherme.marvelcharacters.domain.repository.CharacterRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetFavoriteCharacterById(
+class GetFavoriteCharacterById @Inject constructor(
     private val characterRepository: CharacterRepository
 ) {
 
