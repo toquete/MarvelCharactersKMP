3c77dd5c3c6bf2b213767bf5c6061321b47264cd
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-05-20 17:44:23 -0300

    Criando tela de detalhe em compose

--- app/src/main/java/com/guilherme/marvelcharacters/ui/detail/DetailState.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/ui/detail/DetailState.kt
@@ -1,5 +1,10 @@
 package com.guilherme.marvelcharacters.ui.detail
 
+import com.guilherme.marvelcharacters.domain.model.Character
 import com.guilherme.marvelcharacters.infrastructure.State
 
 data class DetailState(val isFavorite: Boolean = false) : State
+
+data class DetailComposeState(
+    val character: Character? = null
+)
