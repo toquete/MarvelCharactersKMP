7ed26271b32f67dba40743cbc7beb8081f9da94c
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-05-24 18:21:12 -0300

    Extraindo navgraph de detalhe

--- app/src/main/java/com/guilherme/marvelcharacters/navigation/MainNavHost.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/navigation/MainNavHost.kt
@@ -3,12 +3,10 @@ package com.guilherme.marvelcharacters.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.guilherme.marvelcharacters.ui.detail.DetailDestination
-import com.guilherme.marvelcharacters.ui.detail.DetailRoute
+import com.guilherme.marvelcharacters.ui.detail.detailGraph
 import com.guilherme.marvelcharacters.ui.favorites.FavoritesRoute
 import com.guilherme.marvelcharacters.ui.home.HomeRoute
 
@@ -33,13 +31,8 @@ fun MainNavHost(
                 onNavigateToDetail = { navController.navigate("${DetailDestination.route}/$it") }
             )
         }
-        composable(
-            route = "${DetailDestination.route}/{${DetailDestination.characterIdArg}}",
-            arguments = listOf(
-                navArgument(DetailDestination.characterIdArg) { type = NavType.IntType }
-            )
-        ) {
-            DetailRoute(onBackClick = navController::popBackStack)
+        detailGraph {
+            navController.popBackStack()
         }
     }
 }
--- app/src/main/java/com/guilherme/marvelcharacters/ui/detail/DetailDestination.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/ui/detail/DetailDestination.kt
@@ -1,8 +1,25 @@
 package com.guilherme.marvelcharacters.ui.detail
 
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.guilherme.marvelcharacters.navigation.Destination
 
 object DetailDestination : Destination {
     override val route = "detail"
     const val characterIdArg = "characterId"
+}
+
+fun NavGraphBuilder.detailGraph(
+    onBackClick: () -> Unit
+) {
+    composable(
+        route = "${DetailDestination.route}/{${DetailDestination.characterIdArg}}",
+        arguments = listOf(
+            navArgument(DetailDestination.characterIdArg) { type = NavType.IntType }
+        )
+    ) {
+        DetailRoute(onBackClick = onBackClick)
+    }
 }
