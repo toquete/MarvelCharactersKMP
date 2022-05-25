91c20ffd8e46884e85be0ade81efa3dada4255a9
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-05-25 10:40:25 -0300

    Removendo BaseViewModel de HomeViewModel

--- app/src/main/java/com/guilherme/marvelcharacters/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/guilherme/marvelcharacters/ui/home/HomeViewModel.kt
@@ -1,14 +1,17 @@
 package com.guilherme.marvelcharacters.ui.home
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.guilherme.marvelcharacters.BuildConfig
 import com.guilherme.marvelcharacters.R
 import com.guilherme.marvelcharacters.domain.usecase.GetCharactersUseCase
-import com.guilherme.marvelcharacters.infrastructure.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -16,16 +19,19 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getCharactersUseCase: GetCharactersUseCase
-) : BaseViewModel<HomeState, HomeEvent>(HomeState()) {
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(HomeState())
+    val state = _state.asStateFlow()
 
     fun onSearchCharacter(character: String) {
         viewModelScope.launch {
             getCharactersUseCase(character, BuildConfig.MARVEL_KEY, BuildConfig.MARVEL_PRIVATE_KEY)
-                .onStart { setState { it.copy(isLoading = true) } }
-                .onCompletion { setState { it.copy(isLoading = false) } }
+                .onStart { _state.update { it.copy(isLoading = true) } }
+                .onCompletion { _state.update { it.copy(isLoading = false) } }
                 .catch { error ->
                     // TODO: melhorar tratativa de erro
-                    setState {
+                    _state.update {
                         it.copy(
                             errorMessageId = if (error is HttpException) {
                                 R.string.request_error_message
@@ -36,7 +42,7 @@ class HomeViewModel @Inject constructor(
                     }
                 }
                 .collect { list ->
-                    setState { state ->
+                    _state.update { state ->
                         state.copy(
                             characters = list,
                             errorMessageId = if (list.isEmpty()) R.string.empty_state_message else null
