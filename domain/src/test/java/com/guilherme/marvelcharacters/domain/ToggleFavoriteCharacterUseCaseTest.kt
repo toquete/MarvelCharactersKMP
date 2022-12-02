08315e87438d0f2b1787dc74634609557be1cc07
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-02 12:15:32 -0300

    Adicionando testes para ToggleFavoriteCharacterUseCase

---
+++ domain/src/test/java/com/guilherme/marvelcharacters/domain/ToggleFavoriteCharacterUseCaseTest.kt
@@ -0,0 +1,41 @@
+package com.guilherme.marvelcharacters.domain
+
+import com.guilherme.marvelcharacters.data.repository.CharacterRepository
+import com.guilherme.marvelcharacters.domain.usecase.ToggleFavoriteCharacterUseCase
+import io.mockk.MockKAnnotations
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ToggleFavoriteCharacterUseCaseTest {
+
+    @RelaxedMockK
+    private lateinit var repository: CharacterRepository
+
+    @InjectMockKs
+    private lateinit var useCase: ToggleFavoriteCharacterUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `invoke - call deletion when character is favorite`() = runBlockingTest {
+        useCase(id = 0, isFavorite = true)
+
+        coVerify { repository.deleteFavoriteCharacter(id = 0) }
+    }
+
+    @Test
+    fun `invoke - call insertion when character is not favorite`() = runBlockingTest {
+        useCase(id = 0, isFavorite = false)
+
+        coVerify { repository.insertFavoriteCharacter(id = 0) }
+    }
+}
