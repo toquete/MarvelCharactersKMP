8d8260056769e524b06e87f38c1bab759bb2606f
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-02 10:48:36 -0300

    Criando teste unitário

---
+++ cache/src/test/java/com/guilherme/marvelcharacters/cache/FavoriteCharacterLocalDataSourceImplTest.kt
@@ -0,0 +1,70 @@
+package com.guilherme.marvelcharacters.cache
+
+import com.google.common.truth.Truth.assertThat
+import com.guilherme.marvelcharacters.cache.dao.FavoriteCharacterDao
+import com.guilherme.marvelcharacters.cache.util.Fixtures
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class FavoriteCharacterLocalDataSourceImplTest {
+
+    @RelaxedMockK
+    private lateinit var dao: FavoriteCharacterDao
+
+    @InjectMockKs
+    private lateinit var localDataSource: FavoriteCharacterLocalDataSourceImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `getFavoriteCharacters - return list of characters`() = runBlockingTest {
+        coEvery { dao.getFavoriteCharacters() } returns flowOf(Fixtures.favoriteCharacterEntityList)
+
+        val result = localDataSource.getFavoriteCharacters()
+
+        assertThat(result.first()).containsExactly(Fixtures.character)
+    }
+
+    @Test
+    fun `isCharacterFavorite - return if character is favorite`() = runBlockingTest {
+        coEvery { dao.isCharacterFavorite(id = 0) } returns flowOf(true)
+
+        val result = localDataSource.isCharacterFavorite(id = 0)
+
+        assertThat(result.first()).isTrue()
+    }
+
+    @Test
+    fun `copyFavoriteCharacter - check dao is called`() = runBlockingTest {
+        localDataSource.copyFavoriteCharacter(id = 0)
+
+        coVerify { dao.copyFavoriteCharacter(id = 0) }
+    }
+
+    @Test
+    fun `deleteAll - check dao is called`() = runBlockingTest {
+        localDataSource.deleteAll()
+
+        coVerify { dao.deleteAll() }
+    }
+
+    @Test
+    fun `delete - check dao is called`() = runBlockingTest {
+        localDataSource.delete(id = 0)
+
+        coVerify { dao.delete(id = 0) }
+    }
+}
--- cache/src/test/java/com/guilherme/marvelcharacters/cache/util/Fixtures.kt
+++ cache/src/test/java/com/guilherme/marvelcharacters/cache/util/Fixtures.kt
@@ -1,6 +1,7 @@
 package com.guilherme.marvelcharacters.cache.util
 
 import com.guilherme.marvelcharacters.cache.model.CharacterEntity
+import com.guilherme.marvelcharacters.cache.model.FavoriteCharacterEntity
 import com.guilherme.marvelcharacters.core.model.Character
 
 internal object Fixtures {
@@ -19,7 +20,16 @@ internal object Fixtures {
         thumbnail = "test.jpg"
     )
 
+    val favoriteCharacterEntity = FavoriteCharacterEntity(
+        id = 0,
+        name = "Spider-Man",
+        description = "",
+        thumbnail = "test.jpg"
+    )
+
     val characterList = listOf(character)
 
     val characterEntityList = listOf(characterEntity)
+
+    val favoriteCharacterEntityList = listOf(favoriteCharacterEntity)
 }
