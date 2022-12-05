a16976bf9ac3d7230ec51a41fcd732a883cd76a8
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-05 13:40:34 -0300

    Convertendo arquivo settings.gradle para kotlin script

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+rootProject.name = "Marvel Characters"
+include(":app")
+include(":cache")
+include(":data")
+include(":remote")
+include(":domain")
+include(":core:model")
