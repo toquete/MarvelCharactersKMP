bd68569e6c1ed2c7a573dcab5df0d33483187fdd
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-09 15:57:52 -0300

    Removendo arquivos de favorites em app

--- app/src/main/java/com/guilherme/marvelcharacters/ui/favorites/FavoritesUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.guilherme.marvelcharacters.ui.favorites
-
-import androidx.annotation.StringRes
-import com.guilherme.marvelcharacters.core.model.Character
-
-sealed interface FavoritesUiState {
-    data class Success(val list: List<Character> = emptyList()) : FavoritesUiState
-    data class ShowSnackbar(@StringRes val messageId: Int?) : FavoritesUiState
-}
