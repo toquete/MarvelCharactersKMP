4b5f2beb441f9f430886b1d11ad9f4ad48d4a286
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-09 17:04:42 -0300

    Removendo arquivos não utilizados

--- app/src/debug/java/com/guilherme/marvelcharacters/HiltActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.guilherme.marvelcharacters
-
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HiltActivity : AppCompatActivity()
