0a51f8ab5507d3e1f133752150bfcf26eefbc80f
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-20 11:41:02 -0300

    Convertendo build.gradle ui para kts

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.guilherme.marvelcharacters.core.ui"
+    compileSdk = rootProject.extra["compileSdkVersion"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdkVersion"] as Int
+        targetSdk = rootProject.extra["targetSdkVersion"] as Int
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra["sourceCompatibilityVersion"] as JavaVersion
+        targetCompatibility = rootProject.extra["targetCompatibilityVersion"] as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra["jvmTargetVersion"].toString()
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:${rootProject.extra["coreVersion"]}")
+    api("com.google.android.material:material:${rootProject.extra["materialDesignVersion"]}")
+}
