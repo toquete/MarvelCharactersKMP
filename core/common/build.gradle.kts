1ff277e755889c18761e4f807cc3065fcb62a1a6
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-20 11:27:49 -0300

    Convertendo build.gradle common para kts

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.guilherme.marvelcharacters.core.common"
+    compileSdk = rootProject.extra["compileSdkVersion"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdkVersion"] as Int
+        targetSdk = rootProject.extra["targetSdkVersion"] as Int
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra["sourceCompatibilityVersion"] as JavaVersion
+        targetCompatibility = rootProject.extra["targetCompatibilityVersion"] as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra["jvmTargetVersion"].toString()
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:${rootProject.extra["coreVersion"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycleVersion"]}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${rootProject.extra["coroutinesVersion"]}")
+}
