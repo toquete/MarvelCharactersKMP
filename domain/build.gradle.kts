dbb78b1ab75265fc8fba8f689e4ed1f35551d897
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-20 11:47:06 -0300

    Convertendo build.gradle domain para kts

---
+++ domain/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+apply(from = "$rootDir/tools/jacoco/android.gradle")
+
+android {
+    namespace = "com.guilherme.marvelcharacters.domain"
+    compileSdk = rootProject.extra["compileSdkVersion"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdkVersion"] as Int
+        targetSdk = rootProject.extra["targetSdkVersion"] as Int
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra["sourceCompatibilityVersion"] as JavaVersion
+        targetCompatibility = rootProject.extra["targetCompatibilityVersion"] as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra["jvmTargetVersion"].toString()
+    }
+}
+
+dependencies {
+    implementation(project(":data"))
+    implementation(project(":core:model"))
+
+    implementation("com.google.dagger:hilt-android:${rootProject.extra["hiltVersion"]}")
+    kapt("com.google.dagger:hilt-compiler:${rootProject.extra["hiltVersion"]}")
+
+    testImplementation(project(":core:testing"))
+}
