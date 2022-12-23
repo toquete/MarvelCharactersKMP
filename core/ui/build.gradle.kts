29a2140411c0c3ad6c53cee6eec4f25ff430a255
Guilherme Toquete <guilherme.toquete@gmail.com>
2022-12-23 14:50:09 -0300

    Adicionando dependencias compose

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -18,10 +18,26 @@ android {
     kotlinOptions {
         jvmTarget = rootProject.extra["jvmTargetVersion"].toString()
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["kotlinCompilerExtensionVersion"].toString()
+    }
 }
 
 dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2022.12.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
     implementation("androidx.core:core-ktx:${rootProject.extra["coreVersion"]}")
     api("com.google.android.material:material:${rootProject.extra["materialDesignVersion"]}")
     api("com.google.accompanist:accompanist-themeadapter-material:${rootProject.extra["materialThemeAdapterVersion"]}")
+
+    api("androidx.compose.material:material")
+    api("androidx.compose.ui:ui-tooling-preview")
+    debugApi("androidx.compose.ui:ui-tooling")
+    api("androidx.activity:activity-compose:${rootProject.extra["activityVersion"]}")
+    api("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycleVersion"]}")
 }
