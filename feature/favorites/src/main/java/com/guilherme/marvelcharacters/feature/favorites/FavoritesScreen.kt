5bef2e0b7935888b8fa6f2a55c0d3d38ae161049
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-23 16:13:30 -0300

    Add FavoritesScreen composable for displaying favorite characters

---
+++ feature/favorites/src/main/java/com/guilherme/marvelcharacters/feature/favorites/FavoritesScreen.kt
@@ -0,0 +1,108 @@
+package com.guilherme.marvelcharacters.feature.favorites
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.guilherme.marvelcharacters.core.model.Character
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+internal fun FavoritesRoute(
+    viewModel: FavoritesViewModel = koinViewModel(),
+    onCharacterClick: (character: Character) -> Unit = {},
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    FavoritesScreen(
+        state = state,
+        onCharacterClick = onCharacterClick,
+        onSnackbarShown = viewModel::onSnackbarShown
+    )
+}
+
+@Composable
+internal fun FavoritesScreen(
+    state: FavoritesState,
+    onCharacterClick: (character: Character) -> Unit = {},
+    onSnackbarShown: () -> Unit = {}
+) {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val resources = LocalContext.current.resources
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { contentPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize()
+        ) {
+            items(state.characters) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onCharacterClick.invoke(it) }
+                ) {
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = it.name,
+                        fontSize = 16.sp
+                    )
+                    Divider()
+                }
+            }
+        }
+        LaunchedEffect(state.messageId) {
+            scope.launch {
+                state.messageId?.let {
+                    snackbarHostState.showSnackbar(message = resources.getText(it).toString())
+                }
+                onSnackbarShown.invoke()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoritesPreview() {
+    FavoritesScreen(
+        state = FavoritesState(
+            characters = listOf(
+                Character(
+                    id = 1,
+                    name = "Spider-Man",
+                    description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    thumbnail = ""
+                ),
+                Character(
+                    id = 2,
+                    name = "Hulk",
+                    description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    thumbnail = ""
+                )
+            )
+        )
+    )
+}
