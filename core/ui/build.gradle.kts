3037814b22b1c6c828574f4b47fd885b57ec79d7
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-24 12:48:19 -0300

    Add Kotlin Compose dependencies and update material3 version in build.gradle.kts

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -22,4 +23,10 @@ android {
 dependencies {
     implementation(libs.core.ktx)
     api(libs.material)
+    api(libs.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
---
+++ core/ui/src/main/java/com/guilherme/marvelcharacters/core/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.guilherme.marvelcharacters.core.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
