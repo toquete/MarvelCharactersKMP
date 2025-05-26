bccde818b84bd5dca5e2b48757bc22ac446f716e
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-26 15:32:50 -0300

    Replace Divider with HorizontalDivider in HomeScreen for improved layout consistency

--- feature/home/src/main/java/com/guilherme/marvelcharacters/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/guilherme/marvelcharacters/feature/home/HomeScreen.kt
@@ -17,8 +17,8 @@ import androidx.compose.material.icons.filled.Brightness4
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -145,7 +145,7 @@ internal fun HomeContent(
                                     text = it.name,
                                     fontSize = 16.sp
                                 )
-                                Divider()
+                                HorizontalDivider()
                             }
                         }
                     }
