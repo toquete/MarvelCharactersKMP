e84f11d05a157c2992d4ba88bd0725919fc4a36d
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-26 16:45:31 -0300

    Refactor cache module to remove SharedPreferences in favor of DataStore

--- cache/src/main/java/com/guilherme/marvelcharacters/cache/infrastructure/di/CacheModule.kt
+++ cache/src/main/java/com/guilherme/marvelcharacters/cache/infrastructure/di/CacheModule.kt
@@ -1,6 +1,5 @@
 package com.guilherme.marvelcharacters.cache.infrastructure.di
 
-import android.content.Context
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
@@ -15,12 +14,10 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 private const val DATABASE = "character_database"
-private const val DEFAULT_PREFERENCES = "default_preferences"
 private const val USER_PREFERENCES = "user_preferences"
 
 val cacheModule = module {
     single { Room.databaseBuilder(androidContext(), CharacterDatabase::class.java, DATABASE).build() }
-    single { androidContext().getSharedPreferences(DEFAULT_PREFERENCES, Context.MODE_PRIVATE) }
     single { PreferenceDataStoreFactory.create(produceFile = { androidContext().preferencesDataStoreFile(USER_PREFERENCES) }) }
 
     factory { get<CharacterDatabase>().characterDao() }
