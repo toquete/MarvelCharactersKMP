fd90794a6158a6fad0c13089ea0d4034994c03ca
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-27 11:09:10 -0300

    Add Kotlin Multiplatform support and update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.ktlint)
     alias(libs.plugins.detekt)
     alias(libs.plugins.coveralls)
--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,11 +1,32 @@
 plugins {
-    id("java-library")
-    id("kotlin")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
-apply(from = "$rootDir/tools/jacoco/kotlin.gradle")
+kotlin {
+    androidLibrary {
+        namespace = "com.guilherme.marvelcharacters.core.model"
+        compileSdk = 35
+        minSdk = 24
+    }
 
-java {
-    sourceCompatibility = rootProject.extra["sourceCompatibilityVersion"] as JavaVersion
-    targetCompatibility = rootProject.extra["targetCompatibilityVersion"] as JavaVersion
+    val xcfName = "modelKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
 }
