b7fa1a40bc39c5261dffe9307ed14fcfed68086c
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-28 14:29:48 -0300

    Refactor data module to simplify repository bindings using factoryOf

--- data/src/commonMain/kotlin/com/guilherme/marvelcharacters/data/infrastructure/di/DataModule.kt
+++ data/src/commonMain/kotlin/com/guilherme/marvelcharacters/data/infrastructure/di/DataModule.kt
@@ -4,15 +4,11 @@ import com.guilherme.marvelcharacters.data.repository.CharacterRepository
 import com.guilherme.marvelcharacters.data.repository.CharacterRepositoryImpl
 import com.guilherme.marvelcharacters.data.repository.NightModeRepository
 import com.guilherme.marvelcharacters.data.repository.NightModeRepositoryImpl
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-    factory<CharacterRepository> {
-        CharacterRepositoryImpl(
-            remoteDataSource = get(),
-            localDataSource = get(),
-            favoriteLocalDataSource = get()
-        )
-    }
-    factory<NightModeRepository> { NightModeRepositoryImpl(nightModeLocalDataSource = get()) }
+    factoryOf(::CharacterRepositoryImpl).bind<CharacterRepository>()
+    factoryOf(::NightModeRepositoryImpl).bind<NightModeRepository>()
 }
