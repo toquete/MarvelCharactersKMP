5c4aae804133facd33056babee76a18a1a0d9919
Guilherme Toquete <guilherme.toquete@gmail.com>
2025-05-28 16:29:22 -0300

    Refactor build configuration for Kotlin Multiplatform support and update UI theme files

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,31 +1,54 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.compose.multiplatform)
 }
 
-android {
-    namespace = "com.guilherme.marvelcharacters.core.ui"
-    compileSdk = rootProject.extra["compileSdkVersion"] as Int
+kotlin {
+    androidLibrary {
+        namespace = "com.guilherme.marvelcharacters.core.ui"
+        compileSdk = 35
+        minSdk = 24
+    }
 
-    defaultConfig {
-        minSdk = rootProject.extra["minSdkVersion"] as Int
-    }
-    compileOptions {
-        sourceCompatibility = rootProject.extra["sourceCompatibilityVersion"] as JavaVersion
-        targetCompatibility = rootProject.extra["targetCompatibilityVersion"] as JavaVersion
-    }
-    kotlinOptions {
-        jvmTarget = rootProject.extra["jvmTargetVersion"].toString()
-    }
-}
+    val xcfName = "coreUiKit"
 
-dependencies {
-    implementation(libs.core.ktx)
-    api(libs.material3)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                api(compose.material3)
+                implementation(compose.materialIconsExtended)
+                implementation(compose.ui)
+                implementation(compose.components.uiToolingPreview)
+            }
+        }
+        androidMain {
+            dependencies {
+                implementation(libs.core.ktx)
+                implementation(compose.preview)
+            }
+        }
+    }
 }
